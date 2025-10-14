31eb1b673a1e2206355516a61d3ae0c4f6a4138c
mateogall0 <mateogesede@gmail.com>
2025-10-14 13:30:39 -0300

    hotfix: racing condition
    now each request has a dedicated answer

--- pygeist_client/client.py
+++ pygeist_client/client.py
@@ -7,55 +7,14 @@ import threading
 
 
 class PygeistClient(AsyncMethodHandler):
-    def __init__(self,
-                 unresolved_payload_capacity = 2,
-                 resolved_payload_capacity = 2,
-                 ) -> None:
-        self.c = _adapter._create_client(unresolved_payload_capacity,
-                                         resolved_payload_capacity)
-        self._loop = None
-        self._loop_ready = threading.Event()
-        self._task = None
-        self._thread = None
+    def __init__(self) -> None:
+        self.c = _adapter._create_client(1, 1)
 
     def connect(self,
                 url: str,
                 port: int,
                 ) -> None:
         _adapter._connect_client(self.c, url, port)
-        self._start_process_input()
-
-    async def _process_input(self) -> None:
-        try:
-            while True:
-                await self._loop.run_in_executor(None,
-                                                 _adapter._listen_client_input,
-                                                 self.c)
-                await self._loop.run_in_executor(None,
-                                                 _adapter._process_client_input,
-                                                 self.c)
-        except asyncio.CancelledError:
-            pass
-
-    def _start_process_input(self):
-        if self._loop is None:
-            def loop_thread():
-                self._loop = asyncio.new_event_loop()
-                asyncio.set_event_loop(self._loop)
-                self._loop_ready.set()
-                self._loop.run_forever()
-
-            self._thread = threading.Thread(target=loop_thread, daemon=True)
-            self._thread.start()
-            self._loop_ready.wait()
-
-        if self._task is None:
-            self._task = asyncio.run_coroutine_threadsafe(self._process_input(), self._loop)
-
-    def _stop_process_input(self):
-        if self._task is not None:
-            self._task.cancel()
-            self._task = None
 
     async def _handle(self,
                       method: int,
@@ -73,19 +32,18 @@ class PygeistClient(AsyncMethodHandler):
             body,
         )
 
-        while True:
-            try:
-                return await asyncio.to_thread(
-                    _adapter._get_client_response,
-                    self.c,
-                    req_id,
-                )
-            except FailedResponseProcess:
-                pass  # continue until the server replies
+        await asyncio.to_thread(_adapter._listen_client_input,
+                                self.c)
+        await asyncio.to_thread(_adapter._process_client_input,
+                                self.c)
+        return await asyncio.to_thread(
+            _adapter._get_client_response,
+            self.c,
+            req_id,
+        )
 
     def disconnect(self) -> None:
         _adapter._disconnect_client(self.c)
-        self._stop_process_input()
 
     def __del__(self) -> None:
         self.disconnect()
